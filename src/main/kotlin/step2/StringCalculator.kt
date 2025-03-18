07e9d80db812306d88feb11f8ba13474356787fa
syoun602 <syoun602@gmail.com>
2025-03-18 21:05:24 +0100

    feat: add validations for input length

--- src/main/kotlin/step2/StringCalculator.kt
+++ src/main/kotlin/step2/StringCalculator.kt
@@ -3,9 +3,22 @@ package step2
 class StringCalculator(
     private val input: String,
 ) {
+    private var operands: List<Int> = emptyList()
+    private var operator: List<ArithmeticOperator> = emptyList()
+
     init {
         require(input.isNotBlank()) {
             "Input must not be null or empty."
         }
+
+        val splitInput = input.trim().split(DELIMITER)
+
+        require(splitInput.size >= 3) {
+            "Input is not a valid expression. Must have at least two operands and one operator."
+        }
+    }
+
+    companion object {
+        private const val DELIMITER = " "
     }
 }
--- src/test/kotlin/step2/StringCalculatorTest.kt
+++ src/test/kotlin/step2/StringCalculatorTest.kt
@@ -1,14 +1,32 @@
 package step2
 
+import io.kotest.assertions.throwables.shouldNotThrowAny
 import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.ShouldSpec
 import io.kotest.matchers.string.shouldContain
-import org.junit.jupiter.api.Test
 
-class StringCalculatorTest {
-    @Test
-    fun `input must not be null or empty`() {
-        shouldThrow<IllegalArgumentException> {
-            StringCalculator("")
-        }.message shouldContain "null or empty"
+class StringCalculatorTest : ShouldSpec({
+    context("InputValidation") {
+        should("throw exception if null or empty") {
+            shouldThrow<IllegalArgumentException> {
+                StringCalculator("")
+            }.message shouldContain "null or empty"
+        }
+
+        should("throw exception if split strings are less than 3") {
+            listOf("12 34", "12 +", "+ +").forEach {
+                shouldThrow<IllegalArgumentException> {
+                    StringCalculator(it)
+                }
+            }
+        }
+
+        should("contain at least 3 split strings") {
+            listOf("12 34 56", "12  34 56", "12 + 34", "12 34 +").forEach {
+                shouldNotThrowAny {
+                    StringCalculator(it)
+                }
+            }
+        }
     }
-}
+})
