c345ae61644f7b7b09dfc7bfe710598909b4f7c5
syoun602 <syoun602@gmail.com>
2025-03-18 22:00:57 +0100

    feat: add feature for string parsing and validations

--- src/main/kotlin/step2/StringCalculator.kt
+++ src/main/kotlin/step2/StringCalculator.kt
@@ -3,22 +3,62 @@ package step2
 class StringCalculator(
     private val input: String,
 ) {
-    private var operands: List<Int> = emptyList()
-    private var operator: List<ArithmeticOperator> = emptyList()
+    private var operands: MutableList<Int> = mutableListOf()
+    private var operators: MutableList<ArithmeticOperator> = mutableListOf()
 
     init {
         require(input.isNotBlank()) {
             "Input must not be null or empty."
         }
 
-        val splitInput = input.trim().split(DELIMITER)
+        parseInput(input)
+    }
 
-        require(splitInput.size >= 3) {
+    private fun parseInput(input: String) {
+        val splitInput =
+            input.trim()
+                .split(DELIMITER)
+                .filter { it.isNotBlank() }
+
+        require(splitInput.size >= MINIMUM_SPLIT_LENGTH) {
             "Input is not a valid expression. Must have at least two operands and one operator."
         }
+
+        createExpression(splitInput)
+
+        require(operands.size == operators.size + 1) {
+            "Input is not a valid expression. Check the number of operators."
+        }
+    }
+
+    private fun createExpression(splitInput: List<String>) {
+        splitInput.chunked(2).forEach { pair ->
+            val operand = pair[0]
+            val operator = pair.getOrNull(1)
+
+            parseOperand(operand)
+            operator?.let {
+                parseOperator(it)
+            }
+        }
+    }
+
+    private fun parseOperand(input: String) {
+        runCatching {
+            val value = input.toInt()
+            operands.add(value)
+        }.onFailure {
+            throw IllegalArgumentException("Invalid operand: $input.")
+        }
+    }
+
+    private fun parseOperator(input: String) {
+        val operator = ArithmeticOperator.from(input)
+        operators.add(operator)
     }
 
     companion object {
         private const val DELIMITER = " "
+        private const val MINIMUM_SPLIT_LENGTH = 3
     }
 }
--- src/test/kotlin/step2/StringCalculatorTest.kt
+++ src/test/kotlin/step2/StringCalculatorTest.kt
@@ -3,14 +3,14 @@ package step2
 import io.kotest.assertions.throwables.shouldNotThrowAny
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.ShouldSpec
-import io.kotest.matchers.string.shouldContain
+import io.kotest.matchers.shouldBe
 
 class StringCalculatorTest : ShouldSpec({
     context("InputValidation") {
         should("throw exception if null or empty") {
             shouldThrow<IllegalArgumentException> {
                 StringCalculator("")
-            }.message shouldContain "null or empty"
+            }
         }
 
         should("throw exception if split strings are less than 3") {
@@ -22,10 +22,51 @@ class StringCalculatorTest : ShouldSpec({
         }
 
         should("contain at least 3 split strings") {
-            listOf("12 34 56", "12  34 56", "12 + 34", "12 34 +").forEach {
+            listOf("12 + 56", "12  + 56", "12 + 34", "12 + 34").forEach {
                 shouldNotThrowAny {
                     StringCalculator(it)
                 }
+            }
+        }
+    }
+
+    context("ParseSplitInput") {
+        should("not throw exception if expression is correct") {
+            listOf(
+                "1 + 2",
+                "1 + 2 + 3",
+                "1 + 2 / 2 * 5",
+                "5 - 1 * 2 / 4",
+            ).forEach {
+                shouldNotThrowAny {
+                    StringCalculator(it)
+                }
+            }
+        }
+
+        should("throw exception if expression is wrong") {
+            listOf(
+                "1 + 2 +",
+                "+ 1 + 11 / 2",
+                "10 - 5 * 2 +",
+                "* 1 + 3 / 2 +",
+            ).forEach {
+                shouldThrow<IllegalArgumentException> {
+                    StringCalculator(it)
+                }
+            }
+        }
+    }
+
+    context("Addition") {
+        xshould("add all input values") {
+            listOf(
+                "1 + 4",
+                "2 + 3",
+                "2 + 2 + 1",
+                "4 + 1",
+            ).forEach {
+                StringCalculator(it) shouldBe 5
             }
         }
     }
