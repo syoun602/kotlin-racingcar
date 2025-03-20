c3e2686be4b8a40281abf32e380b750425a9f004
syoun602 <syoun602@gmail.com>
2025-03-20 21:33:55 +0100

    refactor: change namings

--- src/main/kotlin/step3/car/Car.kt
+++ src/main/kotlin/step3/car/Car.kt
@@ -3,8 +3,8 @@ package step3.car
 class Car {
     var position = 0
 
-    fun move(generate: () -> Int) {
-        if (MOVE_THRESHOLD <= generate.invoke()) {
+    fun move(generatePower: () -> Int) {
+        if (ENGINE_THRESHOLD <= generatePower.invoke()) {
             position++
         }
     }
@@ -12,6 +12,6 @@ class Car {
     companion object {
         const val MINIMUM_POWER = 1
         const val MAXIMUM_POWER = 9
-        private const val MOVE_THRESHOLD = 4
+        private const val ENGINE_THRESHOLD = 4
     }
 }
