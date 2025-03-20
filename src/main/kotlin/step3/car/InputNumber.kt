592aa15963afd6c4ddc4d0a2ad03989b8f88d8d8
syoun602 <syoun602@gmail.com>
2025-03-20 21:40:54 +0100

    feat: create VO for input number

---
+++ src/main/kotlin/step3/car/InputNumber.kt
@@ -0,0 +1,16 @@
+package step3.car
+
+class InputNumber(
+    private val value: Int,
+) {
+    init {
+        require(value in MINIMUM_NUMBER_INPUT..MAXIMUM_NUMBER_INPUT) {
+            throw IllegalArgumentException("Invalid input. Value must be between 0 and 10.")
+        }
+    }
+
+    companion object {
+        private const val MINIMUM_NUMBER_INPUT = 0
+        private const val MAXIMUM_NUMBER_INPUT = 10
+    }
+}
---
+++ src/test/kotlin/step3/car/InputNumberTest.kt
@@ -0,0 +1,26 @@
+package step3.car
+
+import io.kotest.assertions.throwables.shouldNotThrowAny
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.ShouldSpec
+import io.kotest.inspectors.forAll
+
+class InputNumberTest : ShouldSpec({
+    context("Create") {
+        should("create object") {
+            listOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10).forAll {
+                shouldNotThrowAny {
+                    InputNumber(it)
+                }
+            }
+        }
+
+        should("throw IllegalArgumentException") {
+            listOf(-2, -1, 11, 12).forAll {
+                shouldThrow<IllegalArgumentException> {
+                    InputNumber(it)
+                }
+            }
+        }
+    }
+})
