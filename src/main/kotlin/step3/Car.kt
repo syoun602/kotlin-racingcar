ee4963a4dc26a554094bbd4ef6460c4048de5996
syoun602 <syoun602@gmail.com>
2025-03-20 21:07:18 +0100

    feat: add moving feature for car

--- src/main/kotlin/step3/Car.kt
+++ src/main/kotlin/step3/Car.kt
@@ -2,4 +2,14 @@ package step3
 
 class Car {
     var position = 0
+
+    fun move(generate: () -> Int) {
+        if (MOVE_THRESHOLD <= generate.invoke()) {
+            position++
+        }
+    }
+
+    companion object {
+        private const val MOVE_THRESHOLD = 4
+    }
 }
--- src/test/kotlin/step3/CarTest.kt
+++ src/test/kotlin/step3/CarTest.kt
@@ -2,6 +2,7 @@ package step3
 
 import io.kotest.assertions.throwables.shouldNotThrowAny
 import io.kotest.core.spec.style.ShouldSpec
+import io.kotest.inspectors.forAll
 import io.kotest.matchers.shouldBe
 
 class CarTest : ShouldSpec({
@@ -14,6 +15,26 @@ class CarTest : ShouldSpec({
 
         should("position of the car be 0") {
             Car().position shouldBe 0
+        }
+    }
+
+    context("Move") {
+        should("move a car") {
+            listOf(4, 5, 6, 7, 8, 9).forAll {
+                val car = Car()
+                car.move { it }
+
+                car.position shouldBe 1
+            }
+        }
+
+        should("not move a car") {
+            listOf(0, 1, 2, 3).forAll {
+                val car = Car()
+                car.move { it }
+
+                car.position shouldBe 0
+            }
         }
     }
 })
